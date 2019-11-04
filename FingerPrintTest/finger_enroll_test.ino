a37cae43a686a1d6718a86266441636fef743c51
varigondavamsi <54438314+varigondavamsi@users.noreply.github.com>
2019-11-04 11:32:13 +0530

    Add files via upload

---
+++ FingerPrintTest/finger_enroll_test.ino
@@ -0,0 +1,225 @@
+/*
+ *The following program is used for enrolling a finger print in the sensor
+ *sending the enrollment details to the firebase which is later used for reterving and identifiying the person
+ *developed by v.vamsi , k prudhvi
+ */
+
+
+
+
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+
+#define FIREBASE_HOST "medidatanew.firebaseio.com"  // appdata base
+#define FIREBASE_AUTH "***************************" // you can find it in the security settings
+#define WIFI_SSID "your wifi name"  //wifi id
+#define WIFI_PASSWORD "wifi password" // enter the wifi password
+
+uint8_t getFingerprintEnroll(int id);
+SoftwareSerial mySerial(12, 13); // 12 - D6 - yellow 
+                                 // 13 - d7 - blue
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()
+{
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to internet"); 
+   Serial.println(WiFi.localIP());
+
+ 
+  finger.begin(57600);
+
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println();
+ 
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop()                     
+{
+
+  Serial.println("Enter ID"); // to identify the person 
+  int id = 0;
+  while (true) {
+    while (! Serial.available());    // to get the user id
+    char c = Serial.read();
+    if (! isdigit(c)) break;
+    id *= 10;
+    id += c - '0';
+  }
+  Serial.print("Enrolling ID #");
+  Serial.println(id);
+  
+
+
+  while (!  getFingerprintEnroll(id) );
+}
+
+uint8_t getFingerprintEnroll(int id) {
+  uint8_t p = -1;
+  Serial.println("Waiting for valid finger to enroll");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+      case FINGERPRINT_OK:
+        Serial.println("Image taken");
+        break;
+      case FINGERPRINT_NOFINGER:
+        Serial.println(".");
+        break;
+      case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        break;
+      case FINGERPRINT_IMAGEFAIL:
+        Serial.println("Imaging error");
+        break;
+      default:
+        Serial.println("Unknown error");
+        break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  Serial.println("Remove finger");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+
+  p = -1;
+  Serial.println("Place same finger again");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+      case FINGERPRINT_OK:
+        Serial.println("Image taken");
+        break;
+      case FINGERPRINT_NOFINGER:
+        Serial.println(".");
+        break;
+      case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        break;
+      case FINGERPRINT_IMAGEFAIL:
+        Serial.println("Imaging error");
+        break;
+      default:
+        Serial.println("Unknown error");
+        break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+
+  // OK converted!
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  
+  String Enroll = String("Users") + String(id); // adding these two everytime creates a new child in fire base
+  
+  Firebase.pushString("fingerprint database" ,(Enroll));  // To get multiple entries in the database
+
+  if (Firebase.failed())
+  {
+    Serial.println(Firebase.error());
+  }
+  Serial.println("sent to online database");
+
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Stored!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not store in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+
+
+    
+}
